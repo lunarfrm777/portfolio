5a8453b2dff31a425159be3b90d1c961b19ba184
lunarfrm777 <photoslune8@gmail.com>
2026-03-23 17:20:13 -0400

    installed tailwindcss postcssfixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,astro}",
   ],
   theme: {
     extend: {},
@@ -11,4 +11,3 @@ module.exports = {
   },
   plugins: [],
 }
-
