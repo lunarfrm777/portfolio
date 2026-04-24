ad58e7d1c5d72352e71f9dabd8dc431132833ce3
lunarfrm777 <photoslune8@gmail.com>
2026-04-24 15:45:26 -0400

    added black text colour bold and underline green to Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -66,8 +66,7 @@ const Experience = () => {
     <div name='experience' className='bg-gradient-to-b from-pink-300 to-orange-500 w-full h-full text-white'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white'>
             <div>
-                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
-                <p className='py-6'>These are the technologies that I have worked with</p>
+                <p className='text-4xl font-bold border-b-4 border-green-500 p-2 inline text-black font-bold'>Experience</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
@@ -77,7 +76,7 @@ const Experience = () => {
                     <div key={id} 
                     className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
                     <img src={src} alt="tech languages used" className='w-20 mx-auto'/>
-                    <p className='mt-4'>{title}</p>
+                    <p className='mt-4 text-black font-bold'>{title}</p>
                 </div>
                 ))}
                 
