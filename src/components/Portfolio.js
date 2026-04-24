9ed581513bf6bf586bd003dc58569362dcce487d
lunarfrm777 <photoslune8@gmail.com>
2026-04-24 15:34:45 -0400

    updated Portfolio component to black text and bold

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -59,7 +59,7 @@ const Portfolio = () => {
     ]
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-orange-500 via-orange to-pink-300 w-full h-full text-white'> 
+    <div name='portfolio' className='bg-gradient-to-b from-orange-500 via-orange to-pink-300 w-full h-full text-black font-bold'> 
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div classname='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-green-500'>Portfolio</p>
