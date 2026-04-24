f7e2b5d0e1b7aee0601f71df9ff64b13112e6819
lunarfrm777 <photoslune8@gmail.com>
2026-04-24 15:51:39 -0400

    updated SocialLinks background green and black for accessibility!

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -44,10 +44,10 @@ const SocialLinks = () => {
                  key={id} 
                  className={
                     // eslint-disable-next-line no-useless-concat
-                    "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
+                    "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-green-500" + " " + style}>
                      <a 
                      href={href} 
-                     className='flex justify-between items-center w-full text-white'
+                     className='flex justify-between items-center w-full text-black'
                      download={download}
                      target='_blank'
                      rel="noreferrer"> 
